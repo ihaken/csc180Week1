45894f00c5feb2ff5b1be8e6772f9f0f6e0e0cae
ihaken <ihaken@gmail.com>
2022-01-17 13:25:09 -0800

    Update Program.cs

--- csc180Week1ColtonIndira/Program.cs
+++ csc180Week1ColtonIndira/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,11 @@ namespace csc180Week1ColtonIndira
         static void Main(string[] args)
         {
             //used operators and escaped characters to piece together multiple strings into one line of code
-            Console.WriteLine("\t\t\t\t\tWeek 1 Lab Assignment" +
-                "\n\t\t\t\tAuthors: Indira Fouch and Colton MacDonald" +
-                "\n\t\tWe used \\n for NewLine, \\t for the tabs, \\ for escape characters, and + operators" +
-                "\n\tInteresting fact about Indira: Born without wisdom teeth, an extra spinal disc and a little different sense of taste"+
+            Console.WriteLine("\t\t\t\t\t\tWeek 1 Lab Assignment" +
+                "\n\t\t\t\t\tAuthors: Indira Fouch and Colton MacDonald" +
+                "\n\t\t\tWe used \\n for NewLine, \\t for the tabs, \\ for escape characters, and + operators" +
+                "\n\t\t"+
+                "\n\tInteresting facts about Indira: Born without wisdom teeth, an extra spinal disc and different sense of taste"+
                 "\nOur \'HelloWorld\' solution locations: \'C: \\Users\\ihake\\source\\repos\\csc180\', and Colton's: \'C:\\Users\\colto\\Desktop\\CSC 180\\HelloNewWorld\\HelloWorld.sln'") ;
             Console.ReadLine();
            
