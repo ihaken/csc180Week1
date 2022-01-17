385ae9f6ca923e8fc45e5dd2731e2626dbdb9541
ihaken <ihaken@gmail.com>
2022-01-17 13:19:05 -0800

    Add project files.

---
+++ csc180Week1ColtonIndira/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csc180Week1ColtonIndira
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //used operators and escaped characters to piece together multiple strings into one line of code
+            Console.WriteLine("\t\t\t\t\tWeek 1 Lab Assignment" +
+                "\n\t\t\t\tAuthors: Indira Fouch and Colton MacDonald" +
+                "\n\t\tWe used \\n for NewLine, \\t for the tabs, \\ for escape characters, and + operators" +
+                "\n\tInteresting fact about Indira: Born without wisdom teeth, an extra spinal disc and a little different sense of taste"+
+                "\nOur \'HelloWorld\' solution locations: \'C: \\Users\\ihake\\source\\repos\\csc180\', and Colton's: \'C:\\Users\\colto\\Desktop\\CSC 180\\HelloNewWorld\\HelloWorld.sln'") ;
+            Console.ReadLine();
+           
+        
+        }
+    }
+}
